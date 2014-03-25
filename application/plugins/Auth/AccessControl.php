477e27023623b58b82bf4eb29e9774714d1d1c60
Stefan <Stefan@192.168.2.100>
2014-03-25 16:46:06 +0100

    Anpassungen

--- application/plugins/Auth/AccessControl.php
+++ application/plugins/Auth/AccessControl.php
@@ -1,134 +1,125 @@
 <?php
-class Application_Plugin_Auth_AccessControl extends Zend_Controller_Plugin_Abstract
-{
-	private $_auth = null;
-	private $_acl = null;
-	public function __construct(Zend_Auth $auth, Zend_Acl $acl)
-	{
-		$this->_auth = $auth;
-		$this->_acl  = $acl;
-	}
-	
-	public static function getUserName() {
-		try {
-			if(isset(Zend_Auth::getInstance()->getStorage()->read()->nickname)) {
-				$username = Zend_Auth::getInstance()->getStorage()->read()->nickname;
-			} else {
-				$username = null;
-			}
-		} catch(Exception $e) {
-			$username = null;
-		}
-		
-		return $username;
-	}
-	
-	
-	public static function getUserID() {
-		try {
-			if(isset(Zend_Auth::getInstance()->getStorage()->read()->id)) {
-				$userid = Zend_Auth::getInstance()->getStorage()->read()->id;
-			} else {
-				$userid = null;
-			}
-		} catch(Exception $e) {
-			$userid = null;
-		}
-		
-		return $userid;
-	}
-	
-	public static function getUserRole() {
-		try {
-			if(isset(Zend_Auth::getInstance()->getStorage()->read()->role)) {
-				$role = Zend_Auth::getInstance()->getStorage()->read()->role;
-			} else {
-				$role = null;
-			}
-		} catch(Exception $e) {
-			$role = null;
-		}
-		
-		return $role;
-	}
 
-	public function routeStartup(Zend_Controller_Request_Abstract $request)
-	{
-		$loginForm = new Application_Model_Forms_LoginForm();
-		if (!$this->_auth->hasIdentity() && $loginForm->isValid($_POST)) {
-			$filter = new Zend_Filter_StripTags();
-			$username = $filter->filter($loginForm->getValue('login_user'));
-			$password = $filter->filter($loginForm->getValue('login_password'));
-			
-			if(!(empty($username) || empty($password))) { 
-				$authAdapter = new Application_Plugin_Auth_AuthAdapter();
-				$authAdapter->setIdentity($username);
-				$authAdapter->setCredential($password);
-				$result = $this->_auth->authenticate($authAdapter);
-				
-				if (!$result->isValid()) { 
-					$messages = $result->getMessages();
-					$message = $messages[0];
-				} else {
-					if($loginForm->getValue('rememberlogin') == 1) {
-						Zend_Session::rememberMe(604800); // 7 Tage Login merken
-					} else {
-						Zend_Session::forgetMe();
-					}
-					
-					$storage = $this->_auth->getStorage();
-        			$storage->write($authAdapter->getResultRowObject(null, 'password'));
-					
-					
-					if(($redirect = $request->getParam('redirect_after_login')) == null) {
-						$redirect = 'serie/index';
-					}
-					
-					Zend_Controller_Action_HelperBroker::getStaticHelper('Redirector')->gotoUrl($redirect);
-				}
+class Application_Plugin_Auth_AccessControl extends Zend_Controller_Plugin_Abstract {
 
-				$registry = Zend_Registry::getInstance();
-				
-			}
-		}
-	}
+    private $_auth = null;
+    private $_acl = null;
+
+    public function __construct(Zend_Auth $auth, Zend_Acl $acl) {
+        $this->_auth = $auth;
+        $this->_acl = $acl;
+    }
+
+    public static function getUserName() {
+        try {
+            if (isset(Zend_Auth::getInstance()->getStorage()->read()->nickname)) {
+                $username = Zend_Auth::getInstance()->getStorage()->read()->nickname;
+            } else {
+                $username = null;
+            }
+        } catch (Exception $e) {
+            $username = null;
+        }
+
+        return $username;
+    }
+
+    public static function getUserID() {
+        try {
+            if (isset(Zend_Auth::getInstance()->getStorage()->read()->id)) {
+                $userid = Zend_Auth::getInstance()->getStorage()->read()->id;
+            } else {
+                $userid = null;
+            }
+        } catch (Exception $e) {
+            $userid = null;
+        }
+
+        return $userid;
+    }
+
+    public static function getUserRole() {
+        try {
+            if (isset(Zend_Auth::getInstance()->getStorage()->read()->role)) {
+                $role = Zend_Auth::getInstance()->getStorage()->read()->role;
+            } else {
+                $role = null;
+            }
+        } catch (Exception $e) {
+            $role = null;
+        }
+
+        return $role;
+    }
+
+    public function routeStartup(Zend_Controller_Request_Abstract $request) {
+        $loginForm = new Application_Model_Forms_UserLoginForm();
+        if (!$this->_auth->hasIdentity() && $loginForm->isValid($_POST)) {
+            $filter = new Zend_Filter_StripTags();
+            $username = $filter->filter($loginForm->getValue('login_user'));
+            $password = $filter->filter($loginForm->getValue('login_password'));
+
+            if (!(empty($username) || empty($password))) {
+                $authAdapter = new Application_Plugin_Auth_AuthAdapter();
+                $authAdapter->setIdentity($username);
+                $authAdapter->setCredential($password);
+                $result = $this->_auth->authenticate($authAdapter);
+
+                if (!$result->isValid()) {
+                    $messages = $result->getMessages();
+                    $message = $messages[0];
+                } else {
+                    if ($loginForm->getValue('rememberlogin') == 1) {
+                        Zend_Session::rememberMe(604800); // 7 Tage Login merken
+                    } else {
+                        Zend_Session::forgetMe();
+                    }
+
+                    $storage = $this->_auth->getStorage();
+                    $storage->write($authAdapter->getResultRowObject(null, 'password'));
 
 
-	public function preDispatch(Zend_Controller_Request_Abstract $request)
-	{ 
-	
-		if ($this->_auth->hasIdentity() &&is_object($this->_auth->getIdentity())) 
-		{
-			$role = $this->_auth->getIdentity()->role;
-		} 
-		else 
-		{
-			$role = 'guest';
-		}
-		$module = $request->getModuleName();
-		$controller = $module.'_'.$request->getControllerName();
-		
-		
-		$action = $request->getActionName();
+                    if (($redirect = $request->getParam('redirect_after_login')) == null) {
+                        $redirect = 'serie/index';
+                    }
 
-		if (!$this->_acl->has($controller)) {
-			$controller = 'default_index';
+                    Zend_Controller_Action_HelperBroker::getStaticHelper('Redirector')->gotoUrl($redirect);
+                }
 
-		} 
-		if (!$this->_acl->isAllowed($role, $controller, $action)) { 
-			if ($this->_auth->hasIdentity()) { 
-				$request->setModuleName('default');
-				$request->setControllerName('error');
-				$request->setActionName('erroracl'); 
-			} else { 
-				$query = http_build_query($request->getQuery());
-				$request->setModuleName('default');
-				$request->setControllerName('user');
-				$request->setActionName('requiredlogin');
-				$request->setParam('redirect_after_login', $request->getControllerName().'/'.$action.'/'.$query);
-			}
-		}     
-	}
+                $registry = Zend_Registry::getInstance();
+            }
+        }
+    }
 
+    public function preDispatch(Zend_Controller_Request_Abstract $request) {
+
+        if ($this->_auth->hasIdentity() && is_object($this->_auth->getIdentity())) {
+            $role = $this->_auth->getIdentity()->role;
+        } else {
+            $role = 'guest';
+        }
+        $module = $request->getModuleName();
+        $controller = $module . '_' . $request->getControllerName();
+
+
+        $action = $request->getActionName();
+
+        if (!$this->_acl->has($controller)) {
+            $controller = 'default_index';
+        }
+        if (!$this->_acl->isAllowed($role, $controller, $action)) {
+            if ($this->_auth->hasIdentity()) {
+                $request->setModuleName('default');
+                $request->setControllerName('error');
+                $request->setActionName('erroracl');
+            } else {
+                $query = http_build_query($request->getQuery());
+                $request->setModuleName('default');
+                $request->setControllerName('user');
+                $request->setActionName('requiredlogin');
+                $request->setParam('redirect_after_login', $request->getControllerName() . '/' . $action . '/' . $query);
+            }
+        }
+    }
 
 }
