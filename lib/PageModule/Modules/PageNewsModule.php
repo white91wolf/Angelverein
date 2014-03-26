885c9d27d068627a5764888f8a0c59f2f2048aaf
Fruchuxs <Fruchuxs@gmail.com>
2014-03-26 20:55:58 +0100

    InlineModule Systeme Added

---
+++ lib/PageModule/Modules/PageNewsModule.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PageNewsModule
+ *
+ * @author FloH
+ */
+class PageNewsModule implements TagModule {
+    protected $_contentTable;
+
+    public function init() {
+        $this->_contentTable = new Application_Model_DbTable_ContentTable();
+    }
+    
+    public function getAsString() {
+        // TODO aus config die type id fuer news holen
+        $this->_contentTable->getAllEntriesByTypeId(1);
+    }
+}
