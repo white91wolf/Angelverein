ab9d0b7e0d7bd94cef7be274102551e3720288ba
Fruchuxs <Fruchuxs@gmail.com>
2014-04-11 20:10:29 +0200

    Application_Plugin_Auth_AuthAdapter now overwrites the _authenticateCreateSelect() method to extend it by an INNER JOIN with the given role-table. The role-name ist mapped to "role".

--- application/plugins/Auth/AuthAdapter.php
+++ application/plugins/Auth/AuthAdapter.php
@@ -1,17 +1,38 @@
 <?php
-class Application_Plugin_Auth_AuthAdapter extends Zend_Auth_Adapter_DbTable 
-{
-	public function __construct()
-	{
-		
-		//$registry = Zend_Registry::getInstance();
-		//parent::__construct($registry->dbAdapter);
-		
-		$dbAdapter = Zend_Db_Table::getDefaultAdapter();
-		parent::__construct($dbAdapter);
-		$this->setTableName('user');
-		$this->setIdentityColumn('username');
-		$this->setCredentialColumn('password');
-		$this->setCredentialTreatment('SHA1(?)'); 
-	}
+
+class Application_Plugin_Auth_AuthAdapter extends Zend_Auth_Adapter_DbTable {
+
+    protected $_roleTable = 'rolle';
+    protected $_roleTableRoleName = 'name';
+    protected $_userTableForeign = 'rolle_id';
+
+    public function __construct() {
+        $dbAdapter = Zend_Db_Table::getDefaultAdapter();
+        parent::__construct($dbAdapter);
+        
+        $this->init();
+    }
+
+    private function init() {
+        $this->setupTable();
+    }
+    
+    private function setupTable() {
+        $this->setTableName('user');
+        $this->setIdentityColumn('username');
+        $this->setCredentialColumn('password');
+        $this->setCredentialTreatment('SHA1(?)');
+    }
+
+    protected function _authenticateCreateSelect() {
+        $select = parent::_authenticateCreateSelect();
+
+        $select->join(
+            array('r' => $this->_roleTable),
+            'r.id = ' . $this->_tableName . '.' . $this->_userTableForeign,
+            array('role' => $this->_roleTableRoleName)
+        );
+        
+        return $select;
+    }
 }
